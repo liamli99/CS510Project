766c61af967c73e60e1dcc6b52ac081e46573958
LiamLi <zl127@illinois.edu>
2024-04-18 20:18:26 -0500

    Add images

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ def recommend1(title, df):
     index = df[df['Title'] == title].index[0]
     recommended_list = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_recipes = []
+    recommended_recipes_images = []
     
     for i in recommended_list[0:5]:
         recommended_recipes.append(df.iloc[i[0]].Title)
+        recommended_recipes_images.append('archive/images/' + df.iloc[i[0]].Image_Name + '.jpg')
     
-    return recommended_recipes
+    return recommended_recipes, recommended_recipes_images
 
 def recommend2(inputValue1, inputValue2, df):
     filtered_df = df.copy()
@@ -45,43 +47,56 @@ def recommend2(inputValue1, inputValue2, df):
 
     recommended_list = filtered_df.sort_values(by='Similarity', ascending=False).head(5)
     recommended_recipes = []
+    recommended_recipes_images = []
     
-    for recipes in recommended_list['Title']:
-        recommended_recipes.append(recipes)
+    for recipes_title in recommended_list['Title']:
+        recommended_recipes.append(recipes_title)
+    for recipes_image in recommended_list['Image_Name']:
+        recommended_recipes_images.append('archive/images/' + recipes_image + '.jpg')
         
-    return recommended_recipes
+    return recommended_recipes, recommended_recipes_images
 
 st.header("Recipe Recommendation System")
 
 selectValue = st.selectbox("Select recipe from dropdown:", recipe_list)
 
 if st.button("Show Recommended Recipes with Similar Ingredients"):
-    recommended_recipes = recommend1(selectValue, data)
+    recommended_recipes, recommended_recipes_images = recommend1(selectValue, data)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_recipes[0])
+        st.image(recommended_recipes_images[0])
     with col2:
         st.text(recommended_recipes[1])
+        st.image(recommended_recipes_images[1])
     with col3:
         st.text(recommended_recipes[2])
+        st.image(recommended_recipes_images[2])
     with col4:
         st.text(recommended_recipes[3])
+        st.image(recommended_recipes_images[3])
     with col5:
         st.text(recommended_recipes[4])
+        st.image(recommended_recipes_images[4])
 
 inputValue1 = st.text_input("Enter ingredients you don't want to cook with:")
 inputValue2 = st.text_input("Enter ingredients you would like to cook with:")
 
 if st.button("Show Recommended Recipes with these Ingredients"):
-    recommended_recipes = recommend2(inputValue1, inputValue2, data)
+    recommended_recipes, recommended_recipes_images = recommend2(inputValue1, inputValue2, data)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_recipes[0])
+        st.image(recommended_recipes_images[0])
     with col2:
         st.text(recommended_recipes[1])
+        st.image(recommended_recipes_images[1])
     with col3:
         st.text(recommended_recipes[2])
+        st.image(recommended_recipes_images[2])
     with col4:
         st.text(recommended_recipes[3])
+        st.image(recommended_recipes_images[3])
     with col5:
         st.text(recommended_recipes[4])
+        st.image(recommended_recipes_images[4])
