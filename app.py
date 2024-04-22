a4b600e6769646771115164f24c95c459e90eb04
LiamLi <zl127@illinois.edu>
2024-04-22 01:07:39 -0500

    Separation

--- app.py
+++ app.py
@@ -1,96 +1,11 @@
 import streamlit as st
 import pickle
-import re
 import requests
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from recommend import recommend1, recommend2
 
 data = pickle.load(open("data.pkl", 'rb'))
 recipe_list = data['Title'].values
-
-####################################################################################################
-# Recommendation Metric
-####################################################################################################
-
-def recommendation_metric(similarity, rating, alpha=0.5, beta=0.5):
-    return alpha * similarity + beta * rating / 100
-
-####################################################################################################
-# Recommendation Functions
-####################################################################################################
-
-def recommend1(title, df):
-    tfidf_vectorizer = TfidfVectorizer()
-    tfidf_matrix = tfidf_vectorizer.fit_transform(df['Title'])
-    similarity = cosine_similarity(tfidf_matrix)
-    
-    index = df[df['Title'] == title].index[0]
-
-    df['Similarity'] = similarity[index]
-    df['Recommendation_Metric'] = recommendation_metric(similarity[index], df['Rating'])
-    recommended_df = df.sort_values(by='Recommendation_Metric', ascending=False).head(5)
-    
-    recommended_recipes_titles = []
-    recommended_recipes_images = []
-    # recommended_recipes_ratings = []
-    recommended_recipes_stats = []
-    recommended_recipes_instructions =[]
-    
-    for recipes_title in recommended_df['Title']:
-        recommended_recipes_titles.append(recipes_title)
-    for recipes_image in recommended_df['Image_Name']:
-        recommended_recipes_images.append('archive/images/' + recipes_image + '.jpg')
-    # for recipes_rating in recommended_df['Rating']:
-    #     recommended_recipes_ratings.append(recipes_rating)
-    for _, row in recommended_df.iterrows():
-        recommended_recipes_stats.append(str(round(row['Similarity'], 4)) + ' | ' + str(row['Rating']) + ' | ' + str(round(row['Recommendation_Metric'], 4)))
-    for recipes_instruction in recommended_df['Instructions']:
-        recommended_recipes_instructions.append(recipes_instruction)
-    
-    return recommended_recipes_titles, recommended_recipes_images, recommended_recipes_stats, recommended_recipes_instructions
-
-def recommend2(inputValue1, inputValue2, df):
-    filtered_df = df.copy()
-
-    def not_include(lst, str):
-        return all(ele not in str for ele in lst)
-    
-    if inputValue1 != "":
-        inputValue1_lst = re.split('[, ]+', inputValue1.lower())
-        mask = filtered_df.apply(lambda row: not_include(inputValue1_lst, row['Cleaned_Ingredients']), axis=1)
-        filtered_df = filtered_df[mask]
-        
-    tfidf_vectorizer = TfidfVectorizer()
-
-    all_texts = filtered_df['Cleaned_Ingredients'].tolist() + [inputValue2]
-    tfidf_matrix = tfidf_vectorizer.fit_transform(all_texts)
-
-    input_vector = tfidf_matrix[-1]
-    similarity = cosine_similarity(tfidf_matrix[:-1], input_vector)
-
-    filtered_df['Similarity'] = similarity
-    filtered_df['Recommendation_Metric'] = recommendation_metric(similarity.squeeze(), filtered_df['Rating'])
-    recommended_df = filtered_df.sort_values(by='Recommendation_Metric', ascending=False).head(5)
-    
-    recommended_recipes_titles = []
-    recommended_recipes_images = []
-    # recommended_recipes_ratings = []
-    recommended_recipes_stats = []
-    recommended_recipes_instructions =[]
-
-    for recipes_title in recommended_df['Title']:
-        recommended_recipes_titles.append(recipes_title)
-    for recipes_image in recommended_df['Image_Name']:
-        recommended_recipes_images.append('archive/images/' + recipes_image + '.jpg')
-    # for recipes_rating in recommended_df['Rating']:
-    #     recommended_recipes_ratings.append(recipes_rating)
-    for _, row in recommended_df.iterrows():
-        recommended_recipes_stats.append(str(round(row['Similarity'], 4)) + ' | ' + str(row['Rating']) + ' | ' + str(round(row['Recommendation_Metric'], 4)))
-    for recipes_instruction in recommended_df['Instructions']:
-        recommended_recipes_instructions.append(recipes_instruction)
-        
-    return recommended_recipes_titles, recommended_recipes_images, recommended_recipes_stats, recommended_recipes_instructions
 
 ####################################################################################################
 # Function 1: Show Recommended Recipes with Similar Ingredients
